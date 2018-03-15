f6e6caee5ac981a55b02aa0f0bf10396a1dad239
m305-41\w401 <frogmonkey0513@gmail.com>
2018-03-15 18:20:52 +0800

    123

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BMI;
+            Console.Write("身高:");
+            double a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("體重:");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            double c = b / ((a / 100) * (a / 100));
+
+            Console.WriteLine("BMI:" + c);
+
+            if(c>24)
+            {
+                Console.WriteLine("過重");
+                Console.ReadLine();
+            }
+            else if (c<24&&c>18)
+            {
+                Console.WriteLine("適中");
+                Console.ReadLine();
+            }
+            else if (c<18)
+            {
+                Console.WriteLine("過輕");
+                Console.ReadLine();
+            }
+      }
+    }
+}
